46be14fddffd95c0833863646858b07611cfdb24
yohei.isshiki <yohei.isshiki@rakuten.com>
2023-07-20 14:42:55 +0900

    gitignore options added. added few dev dependencies for portfolio

---
+++ node_modules/babel-plugin-ignore-import/test/fixtures/import/code.js
@@ -0,0 +1,5 @@
+import test from 'test';
+import test2 from 'test2';
+import test3 from 'test_remove';
+import test3_1 from '../../test/this/test_remove';
+import test4 from 'test4';
---
+++ node_modules/babel-plugin-ignore-import/test/fixtures/require/code.js
@@ -0,0 +1,5 @@
+require('test_remove.js');
+require('/test/path/test_remove.js');
+require('valid.js');
+require('invalid.css');
+require('/path/invalid.css');
