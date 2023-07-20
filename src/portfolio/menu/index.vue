44756f91800d017454ed91aa5e3e5b8e8f1a20f5
yohei.isshiki <yohei.isshiki@rakuten.com>
2023-07-20 17:43:36 +0900

    fontfamilies added

--- src/portfolio/menu/index.vue
+++ src/portfolio/menu/index.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="menu">
     <label for="menu" class="menu__button">
-      <input
-        type="checkbox"
+      <input type="checkbox"
         id="menu"
-        class="menu__button-input"/>
+        class="menu__button-input"
+        v-on:click="toggleMenu"
+        />
       <span class="menu__button-hamburger"></span>
     </label>
+    <menu class="menu__sidebar" :class="{'is-active': isActive}">
+      <h2 class="menu__sidebar-title">
+        edelRitter
+      </h2>
+      <ul class="menu__sidebar-list">
+        <li><a href="#">menu1</a></li>
+        <li><a href="#">menu2</a></li>
+        <li><a href="#">menu3</a></li>
+        <li><a href="#">menu4</a></li>
+      </ul>
+    </menu>
+    <div class="menu__background" :class="{'is-active': isActive}"></div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Menu',
+  data: () => {
+    return {isActive: false}
+  },
+  methods: {
+    toggleMenu () {
+      this.isActive = !this.isActive
+    }
+  }
 }
 </script>
 
