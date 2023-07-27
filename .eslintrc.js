a8080085af72f7f80f79ba2ca5550b7f95a248af
yohei.isshiki <yohei.isshiki@rakuten.com>
2023-07-27 10:37:39 +0900

    added storybook initial setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,19 @@
 const path = require('path');
-
 const OFF = 0;
 const WARN = 1;
 const ERROR = 2;
-
 module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jest: true,
+    jest: true
   },
-  extends: [
-    'plugin:vue/essential',
-    'airbnb-base',
-  ],
+  extends: ['plugin:vue/essential', 'airbnb-base', 'plugin:storybook/recommended'],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  plugins: [
-    'vue',
-  ],
+  plugins: ['vue'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? WARN : OFF,
     'no-debugger': process.env.NODE_ENV === 'production' ? ERROR : OFF,
@@ -28,16 +21,14 @@ module.exports = {
     'no-multiple-empty-lines': OFF,
     'no-new': OFF,
     'no-unused-expressions': OFF,
-    'array-callback-return': OFF,
+    'array-callback-return': OFF
   },
   settings: {
     'import/resolver': {
       alias: {
-        map: [
-          ['@', path.join(__dirname, 'src')],
-        ],
-        extensions: ['.ts', '.js', '.jsx', '.json', '.vue'],
-      },
-    },
-  },
+        map: [['@', path.join(__dirname, 'src')]],
+        extensions: ['.ts', '.js', '.jsx', '.json', '.vue']
+      }
+    }
+  }
 };
