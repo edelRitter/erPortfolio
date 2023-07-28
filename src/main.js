756e0e060cb17014492f561820ca4f321234a9d8
yohei.isshiki <yohei.isshiki@rakuten.com>
2023-07-28 16:52:27 +0900

    smoothscrolling for mousewheel added for pc

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { setupSmoothScroll } from '@/assets/js/smoothscroll/index.js'
 
 /** plugin AOS */
 import AOS from 'aos'
@@ -8,7 +9,14 @@ import 'aos/dist/aos.css'
 /** plugin Masonry */
 import MasonryWall from '@yeger/vue-masonry-wall'
 
+function onDomReady() {
+  setupSmoothScroll()
+}
+
+document.addEventListener('DOMContentLoaded', onDomReady)
+
+
 createApp(App)
- .use(AOS.init())
- .use(MasonryWall)
- .mount("#app");
+  .use(AOS.init())
+  .use(MasonryWall)
+  .mount("#app");
