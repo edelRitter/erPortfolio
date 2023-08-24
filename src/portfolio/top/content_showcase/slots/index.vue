c5ad5cba25110df07b104432d02bc61529e4b585
yohei.isshiki <yohei.isshiki@rakuten.com>
2023-08-24 16:02:29 +0900

    showcase information managed by showcase json data for easier management

--- src/portfolio/top/content_showcase/slots/index.vue
+++ src/portfolio/top/content_showcase/slots/index.vue
@@ -1,60 +1,32 @@
 <template>
-  <div class="portfolio-showcase__item portfolio-showcase__bg-dark">
-    <div class="portfolio-showcase__item-border">
-      <p>01</p>
+  <div v-for="data in myShowcase">
+    <div 
+    class="portfolio-showcase__item" 
+    :class="{'portfolio-showcase__bg-light': data.bg === 'light'}">
+      <div class="portfolio-showcase__item-border">
+        {{ data.id }}
+      </div>
+      <p class="portfolio-showcase__item-img">
+        <img :src="data.image" class="w-100">
+      </p>
+      <p class="portfolio-showcase__item-title">
+        {{ data.title }}
+      </p>
+      <p class="portfolio-showcase__item-text">
+        {{ data.description }}
+      </p>
     </div>
-    <p class="portfolio-showcase__item-img"><img src="@/assets/img/illustration/arknights_ajimu.png" class="w-100"></p>
-    <p class="portfolio-showcase__item-title">
-    Arknights Illustration /<br>Ajimu Angelina
-    </p>
-    <p class="portfolio-showcase__item-text">Illustration</p>
-  </div>
+  </div>  
 </template>
 
 <script>
-import hibiscus from '@/assets/img/illustration/arknights_hibiscus.png';
-import specter from '@/assets/img/illustration/arknights_specter.png';
-import w from '@/assets/img/illustration/arknights_w.png';
-import penance from '@/assets/img/illustration/arknights_penance.png';
-import ajimu from '@/assets/img/illustration/arknights_ajimu.png';
+import json from './showcase_data.json'
 
 export default {
   name: 'showcaseItem',
-  setup() {
-    const items = [
-      {
-        id: '01',
-        image: hibiscus,
-        type: 'artwork',
-        title: 'arknights / hibiscus',
-      },
-      {
-        id: '02',
-        image: specter,
-        type: 'artwork',
-        title: 'arknights / specter',
-      },
-      {
-        id: '03',
-        image: w,
-        type: 'artwork',
-        title: 'arknights / W',
-      },
-      {
-        id: '04',
-        image: penance,
-        type: 'artwork',
-        title: 'arknights / penance',
-      },
-      {
-        id: '05',
-        image: ajimu,
-        type: 'artwork',
-        title: 'arknights / angelina',
-      },
-    ]
-    return { 
-      items,
+  data() {
+    return {
+      myShowcase: json
     }
   },
 }
