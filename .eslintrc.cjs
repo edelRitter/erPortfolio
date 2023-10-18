895dafb0dfebe33308d956bd0b324c3fbfaab2b8
yohei.isshiki <yohei.isshiki@rakuten.com>
2023-10-18 09:28:40 +0900

    eslint added

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,37 +1,44 @@
-const path = require("path");
+const path = require('path');
+
 const OFF = 0;
 const WARN = 1;
 const ERROR = 2;
+
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
+    jest: true,
   },
-  extends: ["plugin:vue/essential", "airbnb-base", "plugin:storybook/recommended"],
+  extends: [
+    'plugin:vue/essential',
+    'airbnb-base',
+  ],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: "module"
+    sourceType: 'module',
   },
-  plugins: ["vue"],
-  ignorePatterns: [
-    "**/node_modules/**",
-    "node_modules"
+  plugins: [
+    'vue',
+    'jest',
   ],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? WARN : OFF,
-    "no-debugger": process.env.NODE_ENV === "production" ? ERROR : OFF,
-    "no-useless-return": OFF,
-    "no-multiple-empty-lines": OFF,
-    "no-new": OFF,
-    "no-unused-expressions": OFF,
-    "array-callback-return": OFF
+    'no-console': process.env.NODE_ENV === 'production' ? WARN : OFF,
+    'no-debugger': process.env.NODE_ENV === 'production' ? ERROR : OFF,
+    'no-useless-return': OFF,
+    'no-multiple-empty-lines': OFF,
+    'no-new': OFF,
+    'no-unused-expressions': OFF,
+    'array-callback-return': OFF,
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       alias: {
-        map: [["@", path.join(__dirname, "src")]],
-        extensions: [".ts", ".js", ".jsx", ".json", ".vue"]
-      }
-    }
-  }
+        map: [
+          ['@', path.join(__dirname, 'src')],
+        ],
+        extensions: ['.ts', '.js', '.jsx', '.json', '.vue'],
+      },
+    },
+  },
 };
